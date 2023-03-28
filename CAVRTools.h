d2fe762a59f3fb1609e3f506fada596a8831a161
mansour9897 <mansour9897@gmail.com>
2023-03-28 11:06:00 +0430

    add data types

--- CAVRTools.h
+++ CAVRTools.h
@@ -4,9 +4,9 @@
 #include "Comminucation/CmdExe.h"
 #include "Debug/Debug.h"
 #include "Strings/StrUtility.h"
+#include "datatype.h"
 
 #define bool char
 #define true 1
 #define false 0
-
 #endif
---
+++ datatype.h
@@ -0,0 +1,24 @@
+#ifndef __datatype_
+#define __datatype_
+
+#pragma region boolean
+//boolean type
+#ifndef bool 
+#define bool char
+#endif
+
+#ifndef true
+#define true 1
+#endif
+
+#ifndef false
+#define false 0
+#endif
+//End of boolean type
+#pragma endregion
+
+#define uint_8 unsigned char;
+#define uint_16 unsigned int;
+#define uint_32 unsigned long int;
+
+#endif /* __datatype_ */
